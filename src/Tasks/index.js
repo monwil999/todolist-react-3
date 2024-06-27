079ed74390cd9148e0d9f9a0d02f845a530a4298
Monika Wilk <monikawilkpr@gmail.com>
2024-06-27 20:13:13 +0200

    section-button-header transparent color added

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -19,7 +19,7 @@ const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
             task.done ? " tasks__content--done" : ""
           }`}
         >
-          {task.content}
+          {task.id} - {task.content} 
         </span>
         <button
           className="tasks__button tasks__button--remove"
