f3f963d84c0199c85b0908ed2c1320005b0b0e12
Monika Wilk <138973629+monwil999@users.noreply.github.com>
2024-08-29 16:45:20 +0200

    Update index.js

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,29 +1,37 @@
-import { useState } from 'react';
+import { useState } from "react";
 import "./style.css";
 
-const Form = ({ addNewTask} ) => {
+const Form = ({ addNewTask }) => {
     const [newTaskContent, setNewTaskContent] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
 
     const onFormSubmit = (event) => {
-      event.preventDefault();
-        if (newTaskContent.trim === "") {
-            return
+        event.preventDefault();
+        
+        const trimmedNewTaskContent = newTaskContent.trim();
+        
+        if (!trimmedNewTaskContent) {
+            setErrorMessage("Nie można dodać pustego zadania.");
+            return;
         }
-      addNewTask(newTaskContent.trim));
-      setNewTaskContent("");
 
+        setErrorMessage(""); // Usuwanie błędu, jeśli istnieje poprawne zadanie
+        addNewTask(trimmedNewTaskContent);
+        setNewTaskContent("");
     };
+
     return (
-      <form className="form" onSubmit={onFormSubmit}> 
-        <input
-        value={newTaskContent}
-         className="form__input"
-        placeholder="Co jest do zrobienia?"
-        onChange={({ target }) => setNewTaskContent(target.value)}
-        />
-        <button className="form__button">Dodaj zadanie</button>
-      </form>
-  )
+        <form className="form" onSubmit={onFormSubmit}>
+            <input
+                value={newTaskContent}
+                className="form__input"
+                placeholder="Co jest do zrobienia?"
+                onChange={({ target }) => setNewTaskContent(target.value)}
+            />
+            <button className="form__button">Dodaj zadanie</button>
+            {errorMessage && <p className="form__error">{errorMessage}</p>}
+        </form>
+    );
 };
 
 export default Form;
