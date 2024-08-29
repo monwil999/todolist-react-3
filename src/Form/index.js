1a17e425c7eeb2e8998e6346834515d7d5b06989
Monika Wilk <138973629+monwil999@users.noreply.github.com>
2024-08-29 16:50:04 +0200

    Update index.js

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,32 +1,37 @@
 import { useState } from 'react';
 import "./style.css";
 
-const Form = ({addNewTask}) => {
+const Form = ({ addNewTask }) => {
     const [newTaskContent, setNewTaskContent] = useState("");
 
     const onFormSubmit = (event) => {
-      event.preventDefault();
+        event.preventDefault();
         
         const trimmedNewTaskContent = newTaskContent.trim();
         
+        console.log("Oryginalna wartość zadania:", newTaskContent);
+        console.log("Wartość zadania po trim():", trimmedNewTaskContent);
+
         if (!trimmedNewTaskContent) {
+            console.log("Puste zadanie, nie dodano.");
             return;
         }
-      addNewTask(trimmedNewTaskContent);
-      setNewTaskContent("");
 
+        addNewTask(trimmedNewTaskContent);
+        setNewTaskContent("");
     };
+
     return (
-      <form className="form" onSubmit={onFormSubmit}> 
-        <input
-        value={newTaskContent}
-         className="form__input"
-        placeholder="Co jest do zrobienia?"
-        onChange={({ target }) => setNewTaskContent(target.value)}
-        />
-        <button className="form__button">Dodaj zadanie</button>
-      </form>
-  )
+        <form className="form" onSubmit={onFormSubmit}> 
+            <input
+                value={newTaskContent}
+                className="form__input"
+                placeholder="Co jest do zrobienia?"
+                onChange={({ target }) => setNewTaskContent(target.value)}
+            />
+            <button className="form__button">Dodaj zadanie</button>
+        </form>
+    );
 };
 
 export default Form;
